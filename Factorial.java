d179ae5654708e1cb4feabb496affd8c86642f64
cct29239-hue <cct29239@gmail.com>
2025-12-23 08:11:25 +0545

    java factorial update

--- Factorial.java
+++ Factorial.java
@@ -1,9 +1,18 @@
+
+import java.util.Scanner;
+
+
 public class Factorial{
 
     public static void main(String []args)
     {
-        int n=10;
-        int Factorial = 1;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the Number: ");
+        int n = sc.nextInt();
+
+        
+        long Factorial = 1;
         for(int i=1;i<=n;i++)
         {
             Factorial = Factorial * i;
