78e591a7eb0e6cd65b65f49c2e680ac72276a65b
cct29239-hue <cct29239@gmail.com>
2025-12-23 07:41:31 +0545

    Initial commit

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+public class Factorial{
+
+    public static void main(String []args)
+    {
+        int n=10;
+        int Factorial = 1;
+        for(int i=1;i<=n;i++)
+        {
+            Factorial = Factorial * i;
+
+        }
+        System.out.println(Factorial);
+    }
+
+}
+
+
